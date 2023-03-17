ca0a5e9ec58c033bbe2ba1aa93725b37f9a2dd01
krwi20 <102562042+krwi20@users.noreply.github.com>
2023-03-17 09:43:51 +0000

    Initial commit

---
+++ CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Kris Wittwer on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
