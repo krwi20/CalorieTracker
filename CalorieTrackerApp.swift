4f08baa6b3d39576a4ba8c1365b405421b09b7e6
krwi20 <102562042+krwi20@users.noreply.github.com>
2023-03-17 10:07:25 +0000

    Setup Firebase

--- CalorieTrackerApp.swift
+++ CalorieTrackerApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct CalorieTrackerApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
